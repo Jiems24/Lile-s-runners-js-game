0eace5fb171c40275d1c891b1da2d9ea75b91d79
Jaume Barriuso <jaume.barriuso@gmail.com>
2026-03-04 13:00:11 +0100

    Fit background and position player on road

--- js/main.js
+++ js/main.js
@@ -1,76 +1,41 @@
 
 class Player {
     constructor() {
-        this.positionX = 20
-        this.positionY = 11
+        this.positionX = 200
+        this.positionY = 60
         this.width = 40
         this.height = 40
-
-        //PARA EL SALTO
-        this.isJumping = false;
-        this.jumpheight = 80;
-        this.jumpspeed = 4;
-        this.groundY = 20;
     }
 }
 
 const playerRed = new Player()
 
 const playerElement = document.querySelector(".player");
-const gameBackground = document.querySelector(".gamebackground");
 
-function renderPlayer() {
-    playerElement.style.left = playerRed.positionX + "px";
-    playerElement.style.bottom = playerRed.positionY + "px";
+const road = document.querySelector(".road");
+
+const trackCount = 4;
+const trackIndex = 1;
+
+function playerSecondTrack(){
+const roadHeight = playerElement.clientHeight;
+const playerHeight = playerElement.clientHeight;
+
+const trackHeight = roadHeight / trackCount;
+
+const top = trackIndex * trackHeight + (trackHeight - playerHeight) / 2;
+
+playerElement.style.left = playerRed.positionX + "px";
+playerElement.style.top = top + "px";
 }
 
-renderPlayer();
+window.addEventListener("load", ()=>{
+playerSecondTrack();
+})
+window.addEventListener("resize",()=>{
+playerSecondTrack();
+})
 
-
-function jump() {
-
-    if (playerRed.isJumping) {
-        return;
-    }
-
-    playerRed.isJumping = true
-
-    const runPosition = playerRed.positionY;
-    const jumpPosition = runPosition + playerRed.jumpheight;
-
-    const upInterval = setInterval(() => {
-        playerRed.positionY += playerRed.jumpspeed;
-
-        if (playerRed.positionY >= jumpPosition) {
-            playerRed.jumpspeed *= -1  // change speed direction
-        } else if (playerRed.positionY <= runPosition) {
-            clearInterval(upInterval);
-            playerRed.isJumping = false
-            playerRed.jumpspeed *= -1 // change speed direction
-        }
-
-        renderPlayer();
-    }, 16);
-}
-
-let introStarted = false;
-
-function startIntroScroll() {
-    if (introStarted) {
-        return introStarted = true;
-    }
-    gameBackground.classList.add("hideNumbers");
-
-}
- startIntroScroll();
-
-document.addEventListener("keyup", (e) => {
-    console.log(e.code)
-    if (e.code === "ArrowUp") {
-       
-        jump();
-    }
-});
 
 /*const obstaclesArr = []
 
